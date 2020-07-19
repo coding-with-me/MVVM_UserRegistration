9f049d0cf01ab85f4e613479ffc48cda4d884600
farhanfahim <dev.farhanfahim@gmail.com>
2020-07-19 16:15:12 +0500

    Initial commit

---
+++ app/src/main/java/com/codingwithme/mvvmuserregistration/ui/authentication/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.codingwithme.mvvmuserregistration.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codingwithme.mvvmuserregistration.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
